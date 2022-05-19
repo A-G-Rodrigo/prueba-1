b16ec96d88b2a7394c4124537ee17586be0b50b5
empleado 2 <agrssanchez@outlook.es>
2022-05-19 18:44:23 +0200

    Se añaden los archivos del footer y del header

---
+++ footer.php
@@ -0,0 +1,13 @@
+<footer>
+    <div class="container">
+        <div class="container">
+            <p>Prueba-1</p>
+        </div>
+        <div class="container">
+            <p>A G Rodrigo</p>
+        </div>
+    </div>
+</footer>
+</body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"></script>
+</html>
